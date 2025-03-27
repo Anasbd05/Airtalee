7e2935171546c54dd4f6d969d2359243e58df8bf
anas bd <anastrying05@gmail.com>
2025-03-27 14:56:10 +0000

    shopping cart added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import {ArrowDownward} from '@mui/icons-material'
+import {ArrowDown} from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,21 +11,7 @@ const Sidebar = () => {
                     className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-800 gap-0.5 "
                 >
                     <span className="text-lg font-bold"> Categories </span>
-
-                    <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="size-5"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                    </span>
+                    <ArrowDownward fontSize='small' />
                 </summary>
 
                 <ul className="mt-2 space-y-1 px-4">
